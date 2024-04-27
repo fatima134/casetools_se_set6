38e9f1eeba1c025cbd7c486543e6d193c3290635
fatima makandar <fatima@gmail.com>
2024-04-27 12:55:45 +0530

    commit

---
+++ casetools_se_set7/src/casetools_se_set7/rectangle.java
@@ -0,0 +1,37 @@
+package casetools_se_set7;
+
+public class rectangle {
+	private double width;
+	private double height;
+	public  rectangle(double width,double height) {
+		this.width=width;
+		this.height=height;
+	}
+	public double getwidth() {
+		return width;
+		
+	}
+	public void setwidth(double width) {
+		this.width=width;
+	}
+	public double getheight() {
+		return height;
+	}
+	public void setheight(double height) {
+		this.height=height;
+	}
+	public static void main(String[]args) {
+		//TODO Auto-generated method stub
+	}
+
+
+
+}
+
+
+
+
+
+
+
+
